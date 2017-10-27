3ceb237e7794de13a18958e438d58557d5fefc71
Christy Lam <cyyllam@gmail.com>
2017-10-27 14:13:04 -0700

    revise comments

--- scripts/logfiles_county_demand.R
+++ scripts/logfiles_county_demand.R
@@ -12,31 +12,35 @@ counties <- c("33", "35", "53", "61")
 
 # patterns to search for in regex format
 pattern.file.str <- "year_\\d+{4}_log.txt"
+pattern.dpsm <- "\\s+DPSM\\sfor\\sarea\\s"
+pattern.end.line <- "DevelopmentProposalSamplingModelBySubarea:\\scompleted."
+pattern.tbl.hdr <- "\\s+building_type_id"
 
-# format run names for column names in table
+# format run names for table
 runs <- lapply(run.dir, function(x) unlist(strsplit(x,"[.]"))[[1]])
 
 # set-up master table (wide form)
 df <- NULL
 
-for (r in 1:length(run.dir)) { # loop through each run
+for (r in 1:length(run.dir)) { # for each run
   log.files.all <- list.files(file.path(base.dir, run.dir[r]), pattern = pattern.file.str)
-  log.files <- unique(grep(paste(years,collapse="|"), log.files.all, value=TRUE))
-  for (l in 1:length(log.files)) { # loop through each log file
+  log.files <- unique(grep(paste(years, collapse = "|"), log.files.all, value=TRUE))
+  for (l in 1:length(log.files)) { # for each log file
     txt <- readLines(file.path(base.dir, run.dir[r], log.files[l]))
     year <- str_extract(log.files[l], "\\d+")
     print(paste("reading", runs[r], str_match(log.files[l], "\\d+"), "log"))
-    for (county in counties) {
-      ind.dpsm <- grep("\\s+DPSM\\sfor\\sarea\\s", txt) # find line indices where 'DPSM for area ' occurs
-      ind.cnty <- grep(paste0("\\s+DPSM\\sfor\\sarea\\s", county), txt) # index of target county
+    for (county in counties) { # for each county
+      ind.dpsm <- grep(pattern.dpsm, txt) # find line indices where 'DPSM for area ' occurs
+      ind.cnty <- grep(paste0(pattern.dpsm, county), txt) # index of target county
       ord.ind.dpsm <- which(ind.cnty == ind.dpsm) # which index is it in ind.dpsm?
-      if (which(ind.cnty == ind.dpsm) != 4) { 
+      # isolate county's set of text
+      if (ord.ind.dpsm != 4) { 
         cnty.txt <- txt[ind.cnty:(ind.dpsm[ord.ind.dpsm + 1]-2)]
       } else {
-        ind.endline <- grep("DevelopmentProposalSamplingModelBySubarea:\\scompleted.", txt)
+        ind.endline <- grep(pattern.end.line, txt)
         cnty.txt <- txt[ind.cnty:(ind.endline-1)]
       }
-      ind.headers <-  grep("\\s+building_type_id", cnty.txt) # find line index where 'building_type_id' occurs
+      ind.headers <-  grep(pattern.tbl.hdr, cnty.txt) # find line index where 'building_type_id' occurs
       headers <- str_split(cnty.txt[ind.headers], "\t") %>% unlist()
       cnty.df <- data.frame(matrix(ncol = length(headers), nrow = 0)) %>% setNames(headers) # initialize empty df
       cnty.tblines <- cnty.txt[(ind.headers+1):length(cnty.txt)]
@@ -56,7 +60,7 @@ for (r in 1:length(run.dir)) { # loop through each run
 
 # tidy df
 df <- df %>% rename(Difference = `Difference(T-C)`, Action = `Action(P-D)`)
-colnames(df)[1] <- 'building_type_id'
+colnames(df)[1] <- 'building_type_id' # rename to trim whitespace
 df <- df %>% 
   mutate_each_(funs(as.character), c('Target', 'Current', 'Difference', 'Proposed', 'Demolished', 'Action')) %>%
   mutate_each_(funs(as.numeric), c('Target', 'Current', 'Difference', 'Proposed', 'Demolished'))
@@ -68,7 +72,7 @@ dflong <- df %>% gather(attribute, value, -building_type_id, -county, -year, -ru
 
 # option to export as .csv
 concat.runs <- paste(runs %>% unlist(), collapse = "_")
-write_csv(df, file.path(out.dir, paste0("county_demand_wide_", concat.runs, ".csv")))
-write_csv(dflong, file.path(out.dir, paste0("county_demand_long_", concat.runs, ".csv")))
+# write_csv(df, file.path(out.dir, paste0("county_demand_wide_", concat.runs, ".csv")))
+# write_csv(dflong, file.path(out.dir, paste0("county_demand_long_", concat.runs, ".csv")))
 
 print("Processing complete")
